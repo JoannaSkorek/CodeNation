eb9b579124b36af2d1c940d9b8c85320c16e9e3f
Joanna Skorek <joannaskorek@admins-imac.home>
2022-10-03 16:24:35 +0100

    second push

---
+++ loops.js
@@ -0,0 +1,92 @@
+// let favHolidayDestinations = [
+//     "Venice, Italy",
+//     "Paris, France",
+//     "Barcelona, Spain"
+// ]
+// for (let i = 0 ; i < favHolidayDestinations.length; i++){
+//     console.log (favHolidayDestinations[i])
+// Activity 1 
+// let favMovie = [
+//         "Spiderman",
+//         "Minions",
+//         "Batman",
+//         "XXX",
+//         "France"
+//     ]
+//     for (let movie = 2; movie < favMovie.length; movie++) { 
+//         console.log(favMovie[movie])
+//     }
+
+// for (let i = 0; i <= 10; i ++){
+//     console.log (i +=2)
+// }
+// Activity 2
+// let evenNumber = [18]
+
+// for (let n = evenNumber.length -1; n < 20; n++){
+//     if (n % 2 == 0 ) {
+//         (evenNumber.push(n))
+        
+//     }
+// }
+// console.log (`The even number between 0 and 20 are; ${evenNumber}`)
+// Activity 3 
+// let oddNumber = [0]
+
+// for (let n = 1; n < 20; n++){
+//     if (n % 2!= 0 ) {
+//         (oddNumber.push(n))
+        
+//     }
+// }
+// console.log (`The even number between 0 and 20 are; ${oddNumber}`)
+
+
+
+
+
+// // let evenNumber = [2]
+
+// // for (let n = 0; n < 20; n ++){
+// //     if (n % 2 == 0 ){ 
+// //         (evenNumber.reverse(n))
+// //     }
+// // }
+// // console.log (`The even number between 0 and 20 are; ${evenNumber}`)
+
+
+
+
+
+
+
+// DICE
+// let currentDiceRoll = "5"
+
+// while (currentDiceRoll != 1){
+//     console.log (currentDiceRoll)
+//     currentDiceRoll = Math.floor(Math.random()*6)+1
+// }
+// console.log(currentDiceRoll)
+
+
+// ACTIVITY 3 while loop
+
+// let checkedAge = "25"
+
+// while (checkedAge != 25 ) {
+//     console.log(checkedAge)
+// }
+
+// Activity 4 
+
+// let randomNumber = "4"
+
+// while( randomNumber !=0) {
+//  console.log (randomNumber)
+//  randomNumber = Math.floor(Math.random()*4)
+
+// }
+// console.log (randomNumber)
+
+Activity 5 
---
+++ objects.js
@@ -0,0 +1,62 @@
+// // object  {}
+
+// const person = {
+//     firstName: "Sally",
+//     lastName: "Evans",
+//     age: 27,
+//     occupation: "Sale assistant",
+//     married: false,
+//     homeOwner: false,
+//     hobbies: ["tennis", "gardening", "bungee jumping"],
+//     marketingOp() {
+//         if (this.homeOwner == false && this.age >25) {
+//             return "Send mortgage offer email."
+//         }
+//         else if (this.homeOwner == false && this.age <25) { 
+//             return "Send summer holiday fun credit card offer."
+//         } 
+//         else if (this.homeOwner == true && this.age >25) {
+//             return "Pension investment offer."
+//         } 
+//         else {
+//             return "Send sensible Saving offer."
+//         }
+//     }
+// }
+
+// console.log(person.marketingOp())
+
+
+
+// person.homeOwner = "true"
+
+// // object.property
+// person.age
+// console.log(person.firstName)
+// // // console.log(person["name"])
+
+// Activity 1
+// const pet = {
+//     petName: "Cody",
+//     typeOfPet: "Dog",
+//     age: 1
+
+// }
+
+// console.log(`My ${pet.typeOfPet} is called ${pet.petName}, he is ${pet.age} year old. `)
+   
+
+
+Activity 2
+
+const pet = {
+    petName: "Cody",
+    typeOfPet: "Dog",
+    age: 1, 
+    colour: "black"
+
+}
+
+
+pet.colour = "white"
+console.log (pet)
