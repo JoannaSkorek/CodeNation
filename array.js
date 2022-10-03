bdd3c4129443d5c88cd0d0678996b451db129318
Joanna Skorek <joannaskorek@admins-imac.home>
2022-10-03 14:31:28 +0100

    second push

---
+++ array.js
@@ -0,0 +1,61 @@
+// let favHolidayDestinations = [ 
+//     "Venice, Italy",
+//     "Paris, France",
+//     "Barcelona, Spain"
+// ]
+// console.log (favHolidayDestinations[1].length)
+// favHolidayDestinations[1]= "Florida, USA"
+// console.log (favHolidayDestinations)
+// console.log(favHolidayDestinations.length)
+// console.log (favHolidayDestinations.pop())
+// console.log( favHolidayDestinations.splice())
+// Activity 1
+
+// let favMovie = [
+//     "Spider",
+//     "Minions",
+//     "Batman"
+// ]
+// console.log(favMovie)
+
+
+// Activity 2
+// let favMovie = [
+//        "Spiderman",
+//        "Minions",
+//         "Batman"
+    
+// ]
+// console.log (favMovie[0])
+
+
+// // Activity 3
+
+// let favMovie = [
+//     "Spiderman",
+//     "Minions",
+//     "Batman"
+// ]
+// favMovie [1] = "XXX"
+// console.log (favMovie)
+
+
+//  Activity 4 
+
+// let  favMovie = [
+//         "Spiderman",
+//         "Minions",
+//         "Batman"
+//     ]
+//     console.log(favMovie.splice(1))
+
+    // Activity 5
+
+// let favSongs = [
+//    "ZZZ",
+//    "BBB",
+//    "CCC",
+//    "OOO",
+//    "KKK"
+// ]
+// console.log (remove = favSongs.slice(4))
